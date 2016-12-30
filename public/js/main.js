13103b66a316cbfab406268e0033c31f68e62dd2
daijiaxin <253138265@qq.com>
2016-12-30 14:01:25 +0800

    updata

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $("#products").click(function(){
-	$('#nav_up').animate({left:'8rem'});
+	$('#nav_up').animate({left:'9rem'});
 });
 $("#products").blur(function(){
-	$('#nav_up').animate({left:'-8rem'});
+	$('#nav_up').animate({left:'-9rem'});
 });

502181532c6918fc33f672d231b456beb4daaae6
zyl <949472497@qq.com>
2017-01-16 12:02:55 +0800

    CareersPage

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function() {
 	$("#products").click(function(){
-		$('#nav_up').animate({left:'9rem'});
+	$('#nav_up').animate({left:'10rem'});
 	});
 	$("#products").blur(function(){
-		$('#nav_up').animate({left:'-9rem'});
+	$('#nav_up').animate({left:'-12rem'});
 	});
 	var width = $(window).width();
 	if (width < 766) {

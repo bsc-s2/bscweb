ccbb33376a823a20fec8f78d759f17786be4ea1c
daijiaxin <253138265@qq.com>
2017-01-16 10:42:29 +0800

    logo

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function() {
 		$('#nav_up').animate({left:'-9rem'});
 	});
 	var width = $(window).width();
-	alert(width);
-	alert(width);
+	if (width < 766) {
+		$('#index_logo').attr('src','{{ site.baseurl }}/public/image/logo_black.png');
+	}
 });

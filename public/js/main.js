7ab338a8187f1cb8e2c24dde1d99142f1d95a645
daijiaxin <253138265@qq.com>
2017-01-16 10:31:55 +0800

    updata

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
-$("#products").click(function(){
-	$('#nav_up').animate({left:'9rem'});
-});
-$("#products").blur(function(){
-	$('#nav_up').animate({left:'-9rem'});
+jQuery(document).ready(function() {
+	$("#products").click(function(){
+		$('#nav_up').animate({left:'9rem'});
+	});
+	$("#products").blur(function(){
+		$('#nav_up').animate({left:'-9rem'});
+	});
+	var width = $(window).width();
+	alert(width);
 });

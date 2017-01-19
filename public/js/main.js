5f1bc124ef7a9c8b877500c6e8739706ad3bf16c
zyl <949472497@qq.com>
2017-01-19 15:21:57 +0800

    batter sidebar

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,4 @@
-jQuery(document).ready(function() {
-	$("#products").click(function(){
-	$('#nav_up').animate({left:'10rem'});
-	});
-	$("#products").blur(function(){
-	$('#nav_up').animate({left:'-12rem'});
-	});
-	var width = $(window).width();
-	if (width < 766) {
-		$('#index_logo').attr('src','../public/image/logo_black.png');
-	}
-	
+jQuery(document).ready(function () {
+
+
 });

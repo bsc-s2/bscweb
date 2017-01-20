07f55ae56458e5624a2ea4657ee77c57df6a9dc8
daijiaxin <253138265@qq.com>
2017-01-20 19:39:19 +0800

    ZYLupdate

--- public/js/main.js
+++ public/js/main.js
@@ -44,5 +44,80 @@ jQuery(document).ready(function () {
 		offsetX: 8,
 		offsetY: 8
 	});
+	
+	var clearTime = null;
+	var $index = 0;
+	var $qianindex = 0;
+	autoPlay();
+	
+	function scrollPlay(){
+		$("#overview_button li").eq($index).addClass("overview_button_hover").siblings().removeClass("overview_button_hover");
+		if($index > $qianindex){
+			$(".overview_slide").eq($qianindex).stop(true,true).animate({"left":"-100%"});
+			$(".overview_slide").eq($index).css("left","100%").stop(true,true).animate({"left":"0%"});
+		}else{
+			$(".overview_slide").eq($qianindex).stop(true,true).animate({"left":"100%"});
+			$(".overview_slide").eq($index).css("left","-100%").stop(true,true).animate({"left":"0%"});
+		}
+	}
+	$("#overview_button li").click(function(){
+		clearInterval(clearTime);
+		$index = $(this).index();
+		if($index != $qianindex){
+			scrollPlay();
+			$qianindex = $index;
+		}
+		autoPlay();
+	});
+	function autoPlay(){
+		clearTime = setInterval(function(){
+			$index++;
+			if($index >1){
+				$index = 0;
+			}			
+			scrollPlay();
+			$qianindex = $index;
+		},4000);
+	}
 
+	var clearTime4 = null;
+	var $index4 = 0;
+	var $qianindex4 = 0;
+	autoPlay4();
+	
+	function scrollPlay4(){
+		var cln_long = $(".products_slide").eq($index4).attr("id");
+		if(cln_long=="cln_long"){
+			$(".products_roll").css("height","400");
+		}else{
+			$(".products_roll").css("height","300");
+		}
+		$("#products_button li").eq($index4).addClass("products_button_hover").siblings().removeClass("products_button_hover");
+		if($index4 > $qianindex4){
+			$(".products_slide").eq($qianindex4).stop(true,true).animate({"left":"-100%"});
+			$(".products_slide").eq($index4).css("left","100%").stop(true,true).animate({"left":"0%"});
+		}else{
+			$(".products_slide").eq($qianindex4).stop(true,true).animate({"left":"100%"});
+			$(".products_slide").eq($index4).css("left","-100%").stop(true,true).animate({"left":"0%"});
+		}
+	}
+	$("#products_button li").click(function(){
+		clearInterval(clearTime4);
+		$index4 = $(this).index();
+		if($index4 != $qianindex4){
+			scrollPlay4();
+			$qianindex4 = $index4;
+		}
+		autoPlay4();
+	});
+	function autoPlay4(){
+		clearTime4 = setInterval(function(){
+			$index4++;
+			if($index4 >3){
+				$index4 = 0;
+			}		
+			scrollPlay4();
+			$qianindex4 = $index4;
+		},4000);
+	}
 });

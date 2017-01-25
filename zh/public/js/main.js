46dd9ce9ab11d04395d07b62f13b7403c600dc4b
drdr xp <drdr.xp@gmail.com>
2017-01-26 00:28:25 +0800

    update zh

--- zh/public/js/main.js
+++ zh/public/js/main.js
@@ -8,8 +8,12 @@ jQuery(document).ready(function () {
 	}
 	$("#contact").parent(".post").parent(".content").parent("body").css("background","url(../../../../public/image/black-bg.jpg) no-repeat");
 	$("#contact").parent(".post").parent(".content").parent("body").css("background-size","cover");
-	var headHeight = ($(".head-img").css("height").slice(0,-2) - 80).toString() +"px";
-	$("p.headimg-box").css("height",headHeight);
+
+        if ($(".head-img").length > 0) {
+            var headHeight = $(".head-img").css("height").slice(0,-2) - 80 +"px";
+            $("p.headimg-box").css("height",headHeight);
+        }
+
 	setInterval(function(){
 		$("#blocksItContainer").css("visibility","visible")
 	},1000);
--- zh/zh-release/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-jekyll b -s . -d zh-release \
-    && git checkout release \
-    && mv zh-release zh \
-    && git add zh \
-    && git commit -m 'update zh'
-

4e15c7e48eb17653c3baf9569412459f07dbe00b
zyl <949472497@qq.com>
2017-02-09 15:20:00 +0800

    add slider.js

--- public/js/slider.js
+++ public/js/slider.js
@@ -6,9 +6,7 @@
     var btnObj = {};
 
     function scrollPlay(o) {
-        console.log("===>>>scrollPlay");
         btnObj.eq($index).addClass(b.btnFocusStyle).siblings().removeClass(b.btnFocusStyle);
-        console.log('window.btnObj', window.btnObj);
         if ($index > $qianindex) {
             o.find('.slider').eq($qianindex).stop(true, true).animate({ 'left': '-100%' });
             o.find('.slider').eq($index).css('left', '100%').stop(true, true).animate({ 'left': '0%' });
@@ -18,13 +16,10 @@
         }
     }
 
-
-
     function autoPlay(o) {
         btnObj = o.next('div.slider-btn').find("ul li");
 
         function setTime() {
-            console.log("setTime===>");
             clearTime = setInterval(function() {
 
                 $index++;
@@ -37,7 +32,6 @@
         }
 
         btnObj.click(function() {
-            console.log("===>>>click");
             clearInterval(clearTime);
             $index = $(this).index();
             if ($index != $qianindex) {
@@ -50,14 +44,10 @@
         setTime();
     }
 
-
     a.fn.sliderPlay = function(g) {
-        console.log("===>>>sliderPlay");
         if (g && typeof g === "object") {
-            console.log("g====object")
             a.extend(b, g);
         }
-        console.log("b==", b);
         autoPlay(a(this));
         return this;
     };

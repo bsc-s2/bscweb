d717555afd39b6294b83305ac5a893c1cdd98e32
zyl <949472497@qq.com>
2017-02-14 10:50:50 +0800

    解决网站统计代码重复

--- public/js/main.js
+++ public/js/main.js
@@ -44,4 +44,21 @@ jQuery(document).ready(function() {
         'disableScrolling': true
     });
     $('.ccx-gd').sliderPlay({ btnFocusStyle: 'ccx_btn_hover', speed: 3000 });
+
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments);
+        };
+        i[r].l = 1 * new Date();
+        a = s.createElement(o);
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-91887510-1', 'auto');
+    ga('send', 'pageview');
+
 });

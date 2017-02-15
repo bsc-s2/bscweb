218f54a29186be1c14ce99a84fa8bcbbbc3ec051
zyl <949472497@qq.com>
2017-02-15 16:44:45 +0800

    slider.js整理

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,62 +1,57 @@
 (function($) {
-    var configObj = { btnFocusStyle: 'slider_button_hover', speed: 3000 };
-    var clearTime = null;
-    var $index = 0;
-    var $qianindex = 0;
+    var configObj = { btnFocusStyle: 'slider_button_hover', residenceTime: 3000 };
+    var myTimer = null;
+    var index = 0;
+    var preIndex = 0;
     var btnObj = {};
 
     function scrollPlay(sliderBox) {
-        btnObj.eq($index).addClass(configObj.btnFocusStyle).siblings().removeClass(configObj.btnFocusStyle);
-        var preObj = sliderBox.find('.slider').eq($qianindex);
-        var indexObj = sliderBox.find('.slider').eq($index);
-        if ($index > $qianindex) {
-            primarySlideAnimate(preObj, indexObj, true);
-        } else {
-            primarySlideAnimate(preObj, indexObj, false);
+        btnObj.eq(index).addClass(configObj.btnFocusStyle).siblings().removeClass(configObj.btnFocusStyle);
+        var preObj = sliderBox.find('.slider').eq(preIndex);
+        var indexObj = sliderBox.find('.slider').eq(index);
+        var direction = true;
+        if (index <= preIndex) {
+            direction = false;
         }
+        primarySlideAnimate(preObj, indexObj, direction);
     }
 
     function primarySlideAnimate(preObj, indexObj, direction) {
         var leftValue = '100%';
-        var leftValueNegative = '-100%';
-        if (direction) {
-            leftValue = '100%';
-            leftValueNegative = '-100%';
-        } else {
+        if (!direction) {
             leftValue = '-100%';
-            leftValueNegative = '100%';
         }
+        var leftValueNegative = (Number(leftValue.substr(0, leftValue.length - 1)) / -1).toString() + "%";
         preObj.stop(true, true).animate({ 'left': leftValueNegative });
         indexObj.css('left', leftValue).stop(true, true).animate({ 'left': '0%' });
     }
 
+    function startTimer(sliderBox) {
+        var picNum = sliderBox.find('.slider').length - 1;
+        myTimer = setInterval(function() {
+
+            index++;
+            if (index > picNum) {
+                index = 0;
+            }
+            scrollPlay(sliderBox);
+            preIndex = index;
+        }, configObj.residenceTime);
+    }
+
     function autoPlay(sliderBox) {
         btnObj = sliderBox.next('div.slider-btn').find("ul li");
-        var picNum = sliderBox.find('.slider').length - 1;
-
-        function setTime() {
-            clearTime = setInterval(function() {
-
-                $index++;
-                if ($index > picNum) {
-                    $index = 0;
-                }
-                scrollPlay(sliderBox);
-                $qianindex = $index;
-            }, configObj.speed);
-        }
 
         btnObj.click(function() {
-            clearInterval(clearTime);
-            $index = $(this).index();
-            if ($index != $qianindex) {
+            clearInterval(myTimer);
+            index = $(this).index();
+            if (index != preIndex) {
                 scrollPlay(sliderBox);
-                $qianindex = $index;
+                preIndex = index;
             }
-            setTime();
+            startTimer(sliderBox);
         });
-
-        setTime();
+        startTimer(sliderBox);
     }
 
     $.fn.sliderPlay = function(myConfig) {

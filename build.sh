93a303fc768934cab2fd719ef8a98aef8c856b84
lan <adwerrd@hotmail.com>
2018-05-15 09:33:45 +0800

    md build sh

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
-#!/bin/sh
+#! /bin/bash
+jekyll b -s . -d dist || exit 1
 
-jekyll b -s . -d zh-release \
-    && git checkout release \
-    && rm -rf zh \
-    && mv zh-release zh \
-    && git add zh \
-    && git commit -m 'update zh'
+cp .gitignore dist/ || exit 1
 
+git add --force dist || exit 1
+tree=$(git write-tree --prefix=dist/) || exit 1
+
+echo tree is $tree
+
+commit=$(
+{
+cat <<-END
+Build release-v2
+END
+} | git commit-tree $tree -p release-v2
+) || exit 1
+
+echo commit is $commit
+
+git update-ref refs/heads/release-v2 $commit || exit 1
+
+git reset --hard HEAD
+
+echo "successfully build branch release-v2. you'll need to push release-v2"

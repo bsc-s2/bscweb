1327976acd021b0af058860ba31ba030935a496d
xiaoyu <852483353@qq.com>
2018-05-18 16:36:51 +0800

    add google analytics

--- public/js/main.js
+++ public/js/main.js
@@ -14,44 +14,34 @@ jQuery(document).ready(function () {
     });
     return o;
   };
+
+  // video show & close
   $('#videoTrigger').click(function (event) {
     event.stopPropagation();
     $("#videoBox").css('display', 'block');
     $('#indexVideo')[0].play();
     ga('send', 'event', 'Videos', 'play', 'Product film');
-
   });
-
   $('#videoClose').click(function () {
     $('#indexVideo')[0].pause();
     $("#videoBox").css('display', 'none');
     var myVideo = document.getElementById("indexVideo");
     ga('send', 'event', 'Videos', 'close', 'Product film', parseInt(myVideo.currentTime));
-
   })
+
+  /* login analytics */
+  $('#login').click(function(event) {
+    ga('send', 'event', 'Login');
+  })
+
+  // lightbox option
   lightbox.option({
     'resizeDuration': 200,
     'wrapAround': true,
     'disableScrolling': true
   });
 
-  $('#squeegee').mousemove(function (event) {
-    var Ev = event || window.event;
-    var pointX = Ev.clientX;
-    var warp = $('#squeegee')[0];
-    var wrapX = warp.offsetLeft;
-    var wrapWidth = warp.offsetWidth;
-    var afterWidth = Ev.clientX - wrapX;
-    var beforeWidth = wrapWidth - afterWidth;
-    afterWidth = afterWidth > 950 ? 950 : afterWidth;
-    beforeWidth = beforeWidth < 40 ? 40 : beforeWidth;
-    $('#squeegee .squeegee-before').width(beforeWidth);
-    $('#squeegee .squeegee-after').width(afterWidth);
-    $('#squeegee .squeegee-handle').css({
-      left: afterWidth
-    })
-  });
-
+  // navbar
   $(document).scroll(function () {
     if ($(document).scrollTop() > 100) {
       $(".navbar").addClass('navbar-with-background');
@@ -60,6 +50,7 @@ jQuery(document).ready(function () {
     };
   });
 
+  // index product
   $(".index-product").mouseenter(function (event) {
     $(this).children().slideUp(200);
     $(this).next().slideDown(200);
@@ -68,6 +59,8 @@ jQuery(document).ready(function () {
     $(this).slideUp(200);
     $(this).prev().children().slideDown(200);
   })
+
+  // register
   $(".register-sumbit-button").click(function () {
     var inputList = $(".form-control") || [];
     var isValid = true;
@@ -91,4 +84,22 @@ jQuery(document).ready(function () {
 			$('.register-form')[0].reset();
 		})
   });
+
+  /* google analytics */
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments);
+    };
+    i[r].l = 1 * new Date();
+    a = s.createElement(o);
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  // UA-119482301-1 是测试跟踪ID
+  ga('create', 'UA-119482301-1', 'auto');
+  ga('send', 'pageview');
+  ga('set', 'userId', 'USER_ID');
 });

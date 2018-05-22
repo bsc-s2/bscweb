5950d325d0b576bb56770615213fae937b22c889
lan <adwerrd@hotmail.com>
2018-05-22 13:20:27 +0800

    Build release-v2

--- README.md
+++ README.md
@@ -1,41 +1,44 @@
 # Baishancloud Official Website
 
+测试地址: http://172.18.2.32
 官网项目，基于Jekyll进行开发。
 
 ## 项目结构
 
 ```
-- _data // 格式化好的数据。例如该目录下有news文件，可以通过site.data.news获取news文件的内容
-  - customers  // 客户
-  - news // 新闻
-- _includes // 类似一个个组件，用{% include learn-more.html %}将组件插入页面中
-  - bottom // 底部
-  - head // 头部
-  - learn-more // 了解更多（表单）
-  - navbar-home // 首页的导航条
-  - navbar-more // 更多的导航条
-  - navbar-news // 新闻动态的导航条
-  - navbar-product // 产品技术的导航条
-  - navbar-solution // 解决方案的导航条
-- _layouts // 布局，模板
-  - default // 默认模板
-- _posts // 一个个页面
-  - 2018-04-08-more
-  - 2018-04-08-news
-  - 2018-04-08-solution-互联网金融
-  - 2018-04-08-solution-企业数字化转型
-  - 2018-04-08-solution-在线教育
-  - 2018-04-08-solution-政府安全
-  - 2018-04-08-solution-新型政务数据共享交换
-  - 2018-04-08-solution-游戏行业
-  - 2018-04-08-solution-航空安全
-  - 2018-04-08-solution-资讯信息安全
-  - 2018-04-08-solution
-  - 2018-04-08-technology-atd
-  - 2018-04-08-technology-cdn-x
-  - 2018-04-08-technology-cwn-x
-  - 2018-04-08-technology-fengchao
-- _site // 一旦Jekyll完成转换，就会将生成的页面放在这里（默认）
+- _data            // 格式化好的数据。例如该目录下有news文件，可以通过site.data.news获取news文件的内容
+  - customers      // 首页客户
+  - index-product  // 首页产品
+  - navbar         // 导航条
+  - news           // 新闻
+- _includes        // 类似一个个组件，用{% include learn-more.html %}将组件插入页面中
+  - bottom         // 底部
+  - head           // 头部
+  - learn-more     // 了解更多（表单）
+  - navbar         // 导航条
+- _layouts         // 布局，模板
+  - default        // 默认模板
+- _site            // 一旦Jekyll完成转换，就会将生成的页面放在这里（默认）
+- index                      // 首页
+- tech                       // 产品技术
+  - atd                      // 产品技术-ATD
+  - cdn                      // 产品技术-CDN
+  - cwn                      // 产品技术-CWN
+  - fengchao                 // 产品技术-蜂巢
+- solution                   // 解决方案
+  - index
+  - aviation-safety          // 解决方案-航空安全
+  - corporate-digitization   // 解决方案-企业数字化转型
+  - game-safety              // 解决方案-游戏安全
+  - government-security      // 解决方案-政府安全
+  - information-security     // 解决方案-资讯信息安全
+  - internet-banking         // 解决方案-互联网金融安全
+  - new-government-affairs   // 解决方案-新型政务数据共享交换
+  - online-education         // 解决方案-在线教育
+- news                       // 新闻动态
+  - index
+- more                       // 更多
+  - index
 - public
   - css
   - fonts
@@ -46,5 +49,18 @@
 ## 开发
 
 ```bash
-$ jekyll serve         # 访问 http://127.0.0.1:4000/zh/
+$ jekyll serve         # 访问 http://127.0.0.1:4000/
+```
+
+## 部署测试
+
+本地需要有release分支
+
+```bash
+$ sh build.sh
+```
+
+```bash
+$ ssh root@172.18.2.32
+$ cd /usr/local/dashboard/web/dashboard && git pull
 ```
--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,105 @@
-jQuery(document).ready(function() {
-    $('#videoTrigger').click(function(event) {
-        event.stopPropagation();
-        $("#videoBox").css('display', 'block');
-        $('#indexVideo')[0].play();
-        ga('send', 'event', 'Videos', 'play', 'Product film');
-        
-    })
-    $('#videoClose').click(function() {
-        $('#indexVideo')[0].pause();
-        $("#videoBox").css('display', 'none');
-        var myVideo = document.getElementById("indexVideo");
-        ga('send', 'event', 'Videos', 'close', 'Product film', parseInt(myVideo.currentTime));
-        
-    })
-    lightbox.option({
-        'resizeDuration': 200,
-        'wrapAround': true,
-        'disableScrolling': true
+jQuery(document).ready(function () {
+  $.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+      if (o[this.name]) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
     });
+    return o;
+  };
 
-    $('#squeegee').mousemove(function (event) {
-        var Ev= event || window.event;
-        var pointX = Ev.clientX
-        var warp = $('#squeegee')[0]
-        var wrapX = warp.offsetLeft
-        var wrapWidth = warp.offsetWidth
-        var afterWidth = Ev.clientX - wrapX
-        var beforeWidth = wrapWidth - afterWidth
-        afterWidth = afterWidth > 950 ? 950 : afterWidth
-        beforeWidth = beforeWidth < 40 ? 40 : beforeWidth
-        $('#squeegee .squeegee-before').width(beforeWidth)
-        $('#squeegee .squeegee-after').width(afterWidth)
-        $('#squeegee .squeegee-handle').css({left: afterWidth})
+  // video show & close
+  $('#videoTrigger').click(function (event) {
+    event.stopPropagation();
+    $("#videoBox").css('display', 'block');
+    $('#indexVideo')[0].play();
+    ga('send', 'event', 'Videos', 'play', 'Product film');
+  });
+  $('#videoClose').click(function () {
+    $('#indexVideo')[0].pause();
+    $("#videoBox").css('display', 'none');
+    var myVideo = document.getElementById("indexVideo");
+    ga('send', 'event', 'Videos', 'close', 'Product film', parseInt(myVideo.currentTime));
+  })
+
+  /* login analytics */
+  $('#login').click(function(event) {
+    ga('send', 'event', 'Login');
+  })
+
+  // lightbox option
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'disableScrolling': true
+  });
+
+  // navbar
+  $(document).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+      $(".navbar").addClass('navbar-with-background');
+    } else {
+      $(".navbar").removeClass('navbar-with-background');
+    };
+  });
+
+  // index product
+  $(".index-product").mouseenter(function (event) {
+    $(this).children().slideUp(200);
+    $(this).next().slideDown(200);
+  })
+  $(".index-product-hover").mouseleave(function (event) {
+    $(this).slideUp(200);
+    $(this).prev().children().slideDown(200);
+  })
+
+  // register
+  $(".register-sumbit-button").click(function () {
+    var inputList = $(".form-control") || [];
+    var isValid = true;
+    // check form status
+    $.each(inputList, function(index, input) {
+      if(input.value.length === 0) {
+        isValid = false;
+        input.setCustomValidity("不能为空");
+      } else {
+        input.setCustomValidity("");
+      }
     })
+    isValid && $.ajax({
+      type: "POST",
+      url: 'https://hooks.slack.com/services/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',
+      data: JSON.stringify({
+        text: '#新客户！ ' + JSON.stringify($('.register-form').serializeObject())
+      })
+    }).success(function() {
+			alert("提交成功");
+			$('.register-form')[0].reset();
+		})
+  });
+
+  /* google analytics */
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments);
+    };
+    i[r].l = 1 * new Date();
+    a = s.createElement(o);
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  // UA-119482301-1 是测试跟踪ID
+  ga('create', 'UA-119482301-1', 'auto');
+  ga('send', 'pageview');
+  ga('set', 'userId', 'USER_ID');
 });
-
-
-(function(i, s, o, g, r, a, m) {
-	i['GoogleAnalyticsObject'] = r;
-	i[r] = i[r] || function() {
-			(i[r].q = i[r].q || []).push(arguments);
-	};
-	i[r].l = 1 * new Date();
-	a = s.createElement(o);
-	m = s.getElementsByTagName(o)[0];
-	a.async = 1;
-	a.src = g;
-	m.parentNode.insertBefore(a, m);
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-ga('create', 'UA-91873933-1', 'auto');
-ga('send', 'pageview');
-ga('set', 'userId', 'USER_ID');
